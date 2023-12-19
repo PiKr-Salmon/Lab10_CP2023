46900754a0599c58a40ad3ac6b0d8e99c519c58c
PiKr-Salmon <piyawat_kr@cmu.ac.th>
2023-12-19 16:49:08 +0700

    lab10_3 complete

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,30 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    int count = 0;
+    float sum,meann,sosq = 0;
+    string textline;
+    ifstream source("score.txt");
+    while (getline(source,textline))
+    {
+        sum += stof(textline);
+        sosq += pow(stof(textline),2);
+        count++;
+    }
+    meann = sum/count;
+    cout << "Number of data = " << count <<endl;
+    cout << setprecision(3);
+    cout << "Mean = " << meann <<endl;
+    cout << "Standard deviation = " << sqrt( (sosq/count) - pow(meann,2) ) << endl;
+    
+  return 0;
+}
 //cout << "Number of data = ";
 //cout << "Mean = ";
 //cout << "Standard deviation = ";
