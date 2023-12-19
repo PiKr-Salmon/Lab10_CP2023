69110790e9e339a71254d602b8e59b692e70681c
PiKr-Salmon <piyawat_kr@cmu.ac.th>
2023-12-19 15:44:57 +0700

    Lab10_2 complete

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -10,8 +10,13 @@ int main (){
 	dest.open("cheerbook_copy.txt");
 	
 	//กรอก Code เฉพาะส่วนนี้ ให้สอดคล้องกับโจทย์และ Code ส่วนอื่น ๆ 
+	string textline;
+	dest << "-------------------- BOOM ---------------------" << "\n";
+	while(getline(source,textline)){
+		dest << textline << "\n";
+	}
+	dest << "-------------------- HA!! ---------------------" << "\n"; 
 	//โดยห้ามแก้ไข Code ในส่วนอื่น ๆ
-
     source.close();
     dest.close();
 	return 0;
